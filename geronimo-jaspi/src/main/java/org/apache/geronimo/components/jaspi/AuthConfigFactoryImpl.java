781b4a55ad475947d9a46293c443940cdf1e2434
David Jencks <djencks@apache.org>
2008-07-11 19:26:03 +0000

    GERONIMO-3417 it builds after move
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/jaspi/trunk@676054 13f79535-47bb-0310-9956-ffa450edef68

--- geronimo-jaspi/src/main/java/org/apache/geronimo/components/jaspi/AuthConfigFactoryImpl.java
+++ geronimo-jaspi/src/main/java/org/apache/geronimo/components/jaspi/AuthConfigFactoryImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.geronimo.security.jaspi;
+package org.apache.geronimo.components.jaspi;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
