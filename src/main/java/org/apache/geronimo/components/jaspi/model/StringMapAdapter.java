5d9dce4a06e98d69def7d8d57f54c0cb22604099
David Jencks <djencks@apache.org>
2008-07-14 07:17:38 +0000

    add a little bit of persistence
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/jaspi/trunk/geronimo-jaspi@676497 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/geronimo/components/jaspi/model/StringMapAdapter.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.geronimo.components.jaspi.model;
+
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * @version $Rev:$ $Date:$
+ */
+public class StringMapAdapter extends XmlAdapter<String, Map<String, String>> {
+    public Map<String, String> unmarshal(String s) throws Exception {
+        Properties properties = new Properties();
+        ByteArrayInputStream in = new ByteArrayInputStream(s.getBytes());
+        properties.load(in);
+        Map<String, String> map = new HashMap<String, String>(properties.size());
+        for (Map.Entry entry: properties.entrySet()) {
+            map.put((String)entry.getKey(), (String)entry.getValue());
+        }
+        return map;
+    }
+
+    public String marshal(Map<String, String> map) throws Exception {
+        Properties properties = new Properties();
+        properties.putAll(map);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        properties.store(out, null);
+        return new String(out.toByteArray());
+    }
+
+}
