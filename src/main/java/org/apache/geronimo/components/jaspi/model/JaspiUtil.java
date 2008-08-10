eebd5bd6f344cc12d7b47e6e6614fd731f1f886c
David Jencks <djencks@apache.org>
2008-08-10 18:52:08 +0000

    a few tweaks
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/jaspi/trunk/geronimo-jaspi@684571 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/geronimo/components/jaspi/model/JaspiUtil.java
+++ src/main/java/org/apache/geronimo/components/jaspi/model/JaspiUtil.java
@@ -20,56 +20,58 @@
 
 package org.apache.geronimo.components.jaspi.model;
 
-import java.util.Map;
 import java.util.Collections;
+import java.util.Map;
 
-import javax.security.auth.message.config.AuthConfigProvider;
-import javax.security.auth.message.config.ClientAuthConfig;
 import javax.security.auth.message.AuthException;
+import javax.security.auth.message.config.AuthConfigProvider;
 import javax.security.auth.message.module.ClientAuthModule;
 import javax.security.auth.message.module.ServerAuthModule;
-import javax.security.auth.callback.CallbackHandler;
 
 import org.apache.geronimo.components.jaspi.ClassLoaderLookup;
 
 /**
  * Convenience methods to wrap various jaspi objects into AuthConfigProvider instances, ready to be registered.
- * @version $Rev:$ $Date:$
+ * @version $Rev$ $Date$
  */
 public class JaspiUtil {
 
     private JaspiUtil() {
     }
 
-    public static AuthConfigProvider wrapClientAuthConfig(ClientAuthConfigType clientAuthConfigType, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wraptAuthConfigProvider(ConfigProviderType configProviderType, ClassLoaderLookup classLoaderLookup) {
+        return new ConfigProviderType.ConfigProviderImpl(configProviderType.getClientAuthConfig(), configProviderType.getServerAuthConfig(), classLoaderLookup);
+    }
+
+    public static AuthConfigProvider wrapClientAuthConfig(ClientAuthConfigType clientAuthConfigType, ClassLoaderLookup classLoaderLookup) throws AuthException {
         Map<String, ClientAuthConfigType> clientAuthConfig = Collections.singletonMap(clientAuthConfigType.getKey(), clientAuthConfigType);
         return new ConfigProviderType.ConfigProviderImpl(clientAuthConfig, Collections.<String, ServerAuthConfigType>emptyMap(), classLoaderLookup);
     }
 
-    public static AuthConfigProvider wrapClientAuthContext(ClientAuthContextType clientAuthContextType, boolean _protected, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wrapClientAuthContext(ClientAuthContextType clientAuthContextType, boolean _protected, ClassLoaderLookup classLoaderLookup) throws AuthException {
         ClientAuthConfigType clientAuthConfigType = new ClientAuthConfigType(clientAuthContextType, _protected);
-        return wrapClientAuthConfig(clientAuthConfigType, classLoaderLookup, callbackHandler);
+        return wrapClientAuthConfig(clientAuthConfigType, classLoaderLookup);
     }
 
-    public static AuthConfigProvider wrapClientAuthModule(String messageLayer, String appContext, String authenticationContextID, AuthModuleType<ClientAuthModule> clientAuthModuleType, boolean _protected, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wrapClientAuthModule(String messageLayer, String appContext, String authenticationContextID, AuthModuleType<ClientAuthModule> clientAuthModuleType, boolean _protected, ClassLoaderLookup classLoaderLookup) throws AuthException {
         ClientAuthContextType clientAuthContextType = new ClientAuthContextType(messageLayer, appContext, authenticationContextID, clientAuthModuleType);
-        return wrapClientAuthContext(clientAuthContextType, _protected, classLoaderLookup, callbackHandler);
+        return wrapClientAuthContext(clientAuthContextType, _protected, classLoaderLookup);
     }
 
 
-    public static AuthConfigProvider wrapServerAuthConfig(ServerAuthConfigType serverAuthConfigType, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wrapServerAuthConfig(ServerAuthConfigType serverAuthConfigType, ClassLoaderLookup classLoaderLookup) throws AuthException {
         Map<String, ServerAuthConfigType> serverAuthConfig = Collections.singletonMap(serverAuthConfigType.getKey(), serverAuthConfigType);
         return new ConfigProviderType.ConfigProviderImpl(Collections.<String, ClientAuthConfigType>emptyMap(), serverAuthConfig, classLoaderLookup);
     }
 
-    public static AuthConfigProvider wrapServerAuthContext(ServerAuthContextType serverAuthContextType, boolean _protected, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wrapServerAuthContext(ServerAuthContextType serverAuthContextType, boolean _protected, ClassLoaderLookup classLoaderLookup) throws AuthException {
         ServerAuthConfigType serverAuthConfigType = new ServerAuthConfigType(serverAuthContextType, _protected);
-        return wrapServerAuthConfig(serverAuthConfigType, classLoaderLookup, callbackHandler);
+        return wrapServerAuthConfig(serverAuthConfigType, classLoaderLookup);
     }
 
-    public static AuthConfigProvider wrapServerAuthModule(String messageLayer, String appContext, String authenticationContextID, AuthModuleType<ServerAuthModule> serverAuthModuleType, boolean _protected, ClassLoaderLookup classLoaderLookup, CallbackHandler callbackHandler) throws AuthException {
+    public static AuthConfigProvider wrapServerAuthModule(String messageLayer, String appContext, String authenticationContextID, AuthModuleType<ServerAuthModule> serverAuthModuleType, boolean _protected, ClassLoaderLookup classLoaderLookup) throws AuthException {
         ServerAuthContextType serverAuthContextType = new ServerAuthContextType(messageLayer, appContext, authenticationContextID, serverAuthModuleType);
-        return wrapServerAuthContext(serverAuthContextType, _protected, classLoaderLookup, callbackHandler);
+        return wrapServerAuthContext(serverAuthContextType, _protected, classLoaderLookup);
     }
 
 }
